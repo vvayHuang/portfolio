350a9ea0f555d320ea2b7cba8ac92e4837c12ee8
vvay <momoopsoops@gmail.com>
2024-09-03 15:59:53 +0800

    update: project link

--- src/components/Prj.vue
+++ src/components/Prj.vue
@@ -13,6 +13,102 @@ import IconArrow from './icons/IconArrow90degDown.vue';
         </template>
       </PrjItem>
       <ul class="list-unstyled mb-0">
+        <!-- <li class="py-2 border-bottom">
+          <div class="row row-cols-2 row-cols-md-3 align-items-center ">
+            <div class="col-lg-6 col">
+              <div class="row">
+                <div class="col-auto"><p class="mb-0">2024</p></div>
+                <div class="col-auto"><p class="mb-0">portfolio</p></div>
+              </div>
+            </div>
+            <div class="col-lg-3 col">
+              <div class="row justify-content-end justify-content-md-start ">
+                <div class="col-auto">
+                  <a
+                    href="https://www.figma.com/design/33ouQ7VysM6A2Falg7FTbO/portfolio1?node-id=180-542&t=0kGKv6FK49qviqKa-1"
+                    target="_blank"
+                    class="link-dark link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-100-hover text-uppercase"
+                    >(figma)</a
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row justify-content-end d-none  d-md-flex ">
+                <div class="col-auto">
+                  <a
+                    href="#"
+                    class="link-dark link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-100-hover d-flex  align-items-center py-2"
+                    ><span>Launch</span>
+                    <span class="text-bg-dark p-2 rounded-circle ms-3 lh-1 ">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-arrow-up-right"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
+                        />
+                      </svg>
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li> -->
+        <!-- <li class="py-2 border-bottom">
+          <div class="row row-cols-2 row-cols-md-3 align-items-center ">
+            <div class="col-lg-6 col">
+              <div class="row">
+                <div class="col-auto"><p class="mb-0">2024</p></div>
+                <div class="col-auto"><p class="mb-0">buddy house</p></div>
+              </div>
+            </div>
+            <div class="col-lg-3 col">
+              <div class="row justify-content-end justify-content-md-start ">
+                <div class="col-auto">
+                  <a
+                    href="https://www.figma.com/design/lgKC4901ViopAmKbNRokKc/buddy-house?node-id=185-634&t=3vUBzEIfKL4hiXEi-1"
+                    target="_blank"
+                    class="link-dark link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-100-hover text-uppercase"
+                    >(figma)</a
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row justify-content-end d-none  d-md-flex ">
+                <div class="col-auto">
+                  <a
+                    href="#"
+                    class="link-dark link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-100-hover d-flex  align-items-center py-2"
+                    ><span>Launch</span>
+                    <span class="text-bg-dark p-2 rounded-circle ms-3 lh-1 ">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-arrow-up-right"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
+                        />
+                      </svg>
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li> -->
         <li class="py-2 border-bottom">
           <div class="row row-cols-2 row-cols-md-3 align-items-center ">
             <div class="col-lg-6 col">
@@ -23,14 +119,6 @@ import IconArrow from './icons/IconArrow90degDown.vue';
             </div>
             <div class="col-lg-3 col">
               <div class="row justify-content-end justify-content-md-start ">
-                <div class="col-auto">
-                  <a
-                    href="https://www.figma.com/file/RX78vjYDu0Wtd3591akmX9/GalaxyBoyz?type=design&t=gKUC1NbEebOuWaj9-6"
-                    target="_blank"
-                    class="link-dark link-offset-2 link-offset-1-hover link-underline link-underline-opacity-0 link-underline-opacity-100-hover text-uppercase"
-                    >(figma)</a
-                  >
-                </div>
                 <div class="col-auto">
                   <a
                     href="https://github.com/vvayHuang/galaxyboyz"
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import liveReload from 'vite-plugin-live-reload'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio/',
   plugins: [
     vue(),
+    liveReload(['./src/*html']),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    // 用哪個頻道
+    port: 8080,
+    // 啟動server開啟的頁面 
+    open : 8080,
   }
 })
