d5ec6badd41cb8227a3bc7bdd3e4c784282289be
vvay <momoopsoops@gmail.com>
2024-09-30 18:17:29 +0800

    add: logo components

---
+++ src/components/Logo.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted } from "vue";
+import gsap from "gsap";
+onMounted(() => {
+  gsap.fromTo(
+    ".logo",
+    { x: -129 },
+    { x: 129, duration: 8, ease: "none", repeat: -1 }
+  );
+});
+</script>
+<template>
+  <div class="bg-dark overflow-hidden">
+    <a class="navbar-brand me-0" href="#">
+      <img class="logo" src="../assets/img/logo.png" alt="Logo" />
+    </a>
+  </div>
+</template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,12 @@
 <script setup>
-import { onMounted } from "vue";
-import gsap from "gsap";
-
-onMounted(() => {
-  gsap.fromTo(
-    ".logo",
-    { x: -129, },
-    { x: 129, duration: 8,  ease: "none" , repeat:-1},
-  );
-});
+import Logo from './Logo.vue';
 </script>
 <template>
-  <div class="fixed-top" style="mix-blend-mode: difference;">
+  <div class="fixed-top" style="mix-blend-mode: difference">
     <nav class="navbar navbar-expand-lg">
       <div class="container">
         <span class="flex-grow-1 d-none d-lg-block text-light">©2024</span>
-        <div class=" bg-dark overflow-hidden ">
-          <a class="navbar-brand me-0" href="#">
-            <img class="logo" src="../assets/img/logo.png" alt="Logo" />
-          </a>
-        </div>
+        <Logo/>
         <ul class="navbar-nav mb-lg-0 flex-row flex-grow-1 justify-content-end">
           <li class="nav-item">
             <a href="#PRJ" class="nav-link link-light px-2 px-md-4">作品</a>
