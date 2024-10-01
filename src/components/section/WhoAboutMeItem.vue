7db0723ab52389de7b6bb6029722f1de4c1c32f1
vvay <momoopsoops@gmail.com>
2024-10-01 18:18:00 +0800

    add: text animation

--- src/components/section/WhoAboutMeItem.vue
+++ src/components/section/WhoAboutMeItem.vue
@@ -8,18 +8,26 @@
     </i>
   </div>
   <slot name="textContent">
-    <p class=" fs-4 fs-6  fw-bold">
-      你好，我是黃俊維。一位熱愛設計的網頁設計師／UI設計師求職者，正在積極尋求相關工作機會。
-    </p>
-    <p class=" fs-4 fs-6  fw-bold mb-4">
-      在參加了多媒體設計職訓班、網頁切版直播班以及 Figma 實戰營等課程中，加深了對 HTML、CSS、JavaScript 等前端技術的理解。同時透過 Figma 實戰營，掌握了從原型設計到交付開發的全流程操作，更了解設計與開發之間的關係，學會了如何將這些技術與設計完美結合，打造出更具互動性和美感的使用者界面。
-    </p>
-    <p class=" fs-4 fs-6  fw-bold mb-4">
-      這些實戰經驗讓我能靈活應用工具，並熟悉從需求分析到設計實施的完整流程。不僅在設計方面，我也具備與開發團隊溝通的能力，能在協作開發與迭代過程中有效理解技術需求並提供合適的設計解決方案，對 UI/UX 設計有了更深入的認識。
-    </p>
-    <p class=" fs-4 fs-6  fw-bold mb-4">
-      期待將這些學習與實踐經驗運用在實際工作中，為公司提供創新和實用的設計解決方案，並在持續學習中為團隊增值。
-    </p>
+    <div class=" overflow-hidden mb-3">
+      <p class=" fs-4 fs-6  fw-bold animate__animated animate__slideInUp animate__slow	1s mb-0">
+        你好，我是黃俊維。一位熱愛設計的網頁設計師／UI設計師求職者，正在積極尋求相關工作機會。
+      </p>
+    </div>
+    <div class=" overflow-hidden mb-3">
+      <p class=" fs-4 fs-6  fw-bold animate__animated animate__slideInUp animate__slow	2s mb-0">
+        在參加了多媒體設計職訓班、網頁切版直播班以及 Figma 實戰營等課程中，加深了對 HTML、CSS、JavaScript 等前端技術的理解。同時透過 Figma 實戰營，掌握了從原型設計到交付開發的全流程操作，更了解設計與開發之間的關係，學會了如何將這些技術與設計完美結合，打造出更具互動性和美感的使用者界面。
+      </p>
+    </div>
+    <div class=" overflow-hidden mb-3">
+      <p class=" fs-4 fs-6  fw-bold animate__animated animate__slideInUp animate__slow	2s mb-0">
+        這些實戰經驗讓我能靈活應用工具，並熟悉從需求分析到設計實施的完整流程。不僅在設計方面，我也具備與開發團隊溝通的能力，能在協作開發與迭代過程中有效理解技術需求並提供合適的設計解決方案，對 UI/UX 設計有了更深入的認識。
+      </p>
+    </div>
+    <div class=" overflow-hidden mb-3">
+      <p class=" fs-4 fs-6  fw-bold animate__animated animate__slideInUp animate__slow	3s mb-0">
+        期待將這些學習與實踐經驗運用在實際工作中，為公司提供創新和實用的設計解決方案，並在持續學習中為團隊增值。
+      </p>
+    </div>
     <img class=" mb-2 img-fluid " src="../../assets/img/personal-img.png" alt="personal img">
     <p class=" small">WAY HUANG (TAKE IN 2022)</p>
   </slot>
