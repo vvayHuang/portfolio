f066347a7a8c01500ad051f61762a48a5d9b13a5
vvay <momoopsoops@gmail.com>
2024-11-09 16:57:26 +0800

    refactor: 💡 modal add nav content
    switch content in modal
    
    BREAKING CHANGE: 🧨 thx chat gpt

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,109 +1,173 @@
 <script setup>
+import { ref } from "vue";
 import IconGithub from "./icons/IconGithub.vue";
 import IconLink45deg from "./icons/IconLink45deg.vue";
 import IconFigma from "./icons/IconFigma.vue";
 import projectsModalData from "../data/projectsModal";
 import IconCircleFill from "./icons/IconCircleFill.vue";
+
+// 預設選中第一個 tab
+const activeTab = ref(projectsModalData[0].module);
+
+// 切換tab
+function changeTab(module) {
+  activeTab.value = module;
+}
 </script>
 <template>
-  <div v-for="(projectsModal, index) in projectsModalData" :key="index">
-    <div
-      class="modal fade"
-      :id="projectsModal.id"
-      tabindex="-1"
-      aria-labelledby="LifeRecordModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog modal-xl modal-dialog-centered">
-        <div class="modal-content overflow-hidden bg-transparent border-0">
-          <div class="modal-header border-0 justify-content-center flex-column align-items-start">
+  <div
+    class="modal fade"
+    id="Modal"
+    tabindex="-1"
+    aria-labelledby="LifeRecordModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-xl modal-dialog-centered">
+      <div class="modal-content overflow-hidden bg-transparent border-0">
+        <button
+          type="button"
+          class="btn-close btn-close-white m-0 mb-4"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        ></button>
+
+        <!-- Nav Pills -->
+        <ul class="bg-light mb-3 nav nav-pills p-1 rounded-5" id="pills-tab" role="tablist">
+          <li
+            v-for="(project, index) in projectsModalData"
+            :key="index"
+            class="nav-item"
+            role="presentation"
+          >
             <button
+              class="nav-link rounded-5"
+              :class="{ active: activeTab === project.module, 'text-bg-dark': activeTab === project.module }"
+              @click="changeTab(project.module)"
+              :id="project.module"
+              data-bs-toggle="pill"
+              :data-bs-target="'#' + project.module"
               type="button"
-              class="btn-close btn-close-white m-0"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-            <div class="container">
-              <div class="row row-cols-md-2 row-cols-1 g-lg-3 g-md-2 gy-2">
-                <div v-for="(images, index) in projectsModal.images"
-                :key="index" class="col">
-                  <img
-                    class="w-100 h-100"
-                    :src="images.src"
-                    :alt="images.alt"
-                  />
+              role="tab"
+              aria-controls="pills-home"
+              aria-selected="true"
+            >
+              {{ project.title }}
+            </button>
+          </li>
+        </ul>
+
+        <!-- Tab Content -->
+        <div class="tab-content" id="pills-tabContent">
+          <div
+            v-for="(project, index) in projectsModalData"
+            :key="index"
+            class="tab-pane fade"
+            :class="{ 'show active': activeTab === project.module }"
+            :id="project.module"
+            role="tabpanel"
+            :aria-labelledby="project.module"
+            tabindex="0"
+          >
+            <div
+              class="modal-header border-0 justify-content-center flex-column align-items-start"
+            >
+              <div class="container">
+                <div class="row row-cols-md-2 row-cols-1 g-lg-3 g-md-2 gy-2">
+                  <div
+                    v-for="(image, index) in project.images"
+                    :key="index"
+                    class="col"
+                  >
+                    <img
+                      class="w-100 h-100"
+                      :src="image.src"
+                      :alt="image.alt"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="modal-body text-bg-light rounded-3">
-            <div class="container">
-              <div class="flex-column flex-md-row justify-content-between row">
-                <div class="col">
-                  <h2 class="text-secondary fs-6">{{ projectsModal.type }}</h2>
-                  <h1 class="modal-title mb-md-3" id="LifeRecordModalLabel">
-                    {{ projectsModal.title }}
-                  </h1>
-                  <!-- icon link -->
-                  <div class="mb-3">
-                    <a
-                      v-if="projectsModal.figma"
-                      :href="projectsModal.figma"
-                      class="link-dark link-opacity-75 link-opacity-100-hover me-3 d-inline"
-                      target="_blank"
-                      ><IconFigma
-                    /></a>
-                    <a
-                      v-if="projectsModal.github"
-                      :href="projectsModal.github"
-                      class="link-dark link-opacity-75 link-opacity-100-hover me-3 d-inline"
-                      target="_blank"
-                      ><IconGithub
-                    /></a>
-                    <a
-                      v-if="projectsModal.webkitURL"
-                      :href="projectsModal.webkitURL"
-                      class="link-dark link-opacity-75 link-opacity-100-hover me-3"
-                      target="_blank"
-                      ><IconLink45deg
-                    /></a>
+
+            <div class="modal-body text-bg-light rounded-3">
+              <div class="container">
+                <div
+                  class="flex-column flex-md-row justify-content-between row"
+                >
+                  <div class="col">
+                    <h2 class="text-secondary fs-6">{{ project.type }}</h2>
+                    <h1 class="modal-title mb-md-3" id="LifeRecordModalLabel">
+                      {{ project.title }}
+                    </h1>
+
+                    <!-- Icon Links -->
+                    <div class="mb-3">
+                      <a
+                        v-if="project.figma"
+                        :href="project.figma"
+                        class="link-dark me-3"
+                        target="_blank"
+                      >
+                        <IconFigma />
+                      </a>
+                      <a
+                        v-if="project.github"
+                        :href="project.github"
+                        class="link-dark me-3"
+                        target="_blank"
+                      >
+                        <IconGithub />
+                      </a>
+                      <a
+                        v-if="project.webkitURL"
+                        :href="project.webkitURL"
+                        class="link-dark"
+                        target="_blank"
+                      >
+                        <IconLink45deg />
+                      </a>
+                    </div>
+
+                    <!-- Language List -->
+                    <ul class="list-unstyled d-inline-flex flex-wrap">
+                      <li
+                        v-for="(language, index) in project.language"
+                        :key="index"
+                        class="d-flex align-items-center me-3"
+                      >
+                        <IconCircleFill class="me-3" />
+                        <small class="text-muted me-1 text-uppercase">{{
+                          language.label
+                        }}</small>
+                        <small class="text-muted me-1 text-uppercase">{{
+                          language.value + "%"
+                        }}</small>
+                      </li>
+                    </ul>
+
+                    <!-- Tags -->
+                    <div class="mb-3">
+                      <span
+                        v-for="(tag, index) in project.tag"
+                        :key="index"
+                        class="badge rounded-pill text-bg-dark me-2 text-capitalize"
+                      >
+                        {{ tag }}
+                      </span>
+                    </div>
                   </div>
-                  <ul class="list-unstyled d-inline-flex flex-wrap">
-                    <li
-                      v-for="(language, index) in projectsModal.language"
-                      :key="index"
-                      class="d-flex align-items-center me-3"
-                    >
-                      <IconCircleFill class="me-3"/>
-                      <small class="text-muted me-1 text-uppercase">{{
-                        language.label
-                      }}</small>
-                      <small class="text-muted me-1 text-uppercase">{{
-                        language.value + "%"
-                      }}</small>
-                    </li>
-                  </ul>
-                  <!-- tag -->
-                  <div class="mb-3">
-                    <span
-                      v-for="(tag, index) in projectsModal.tag"
-                      :key="index"
-                      class="badge rounded-pill text-bg-dark me-2 text-capitalize"
-                      >{{ tag }}</span
-                    >
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="d-flex flex-column h-100">
-                    <p>
-                      {{ projectsModal.textContent }}
-                    </p>
+
+                  <!-- Text Content -->
+                  <div class="col">
+                    <div class="d-flex flex-column h-100">
+                      <p>{{ project.textContent }}</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <!-- End of Tab Content -->
       </div>
     </div>
   </div>
--- src/data/projectsList.js
+++ src/data/projectsList.js
@@ -3,7 +3,7 @@ const projectsList = [
     id: 1,
     name: "GalaxyBoyz 2.0",
     year: 2024,
-    module:"#GbModal2",
+    module:"#Modal",
     figma:
       "https://www.figma.com/design/RX78vjYDu0Wtd3591akmX9/GalaxyBoyz?m=auto&t=1tlrYtTjsZWEFB0q-6",
     github: "https://github.com/vvayHuang/galaxyBoyzV3",
@@ -13,7 +13,7 @@ const projectsList = [
     id: 2,
     name: "GalaxyBoyz 1.0",
     year: 2023,
-    module:"#GbModal1",
+    module:"#Modal",
     
     github: "https://github.com/vvayHuang/galaxyboyz",
     webkitURL: "https://vvayhuang.github.io/galaxyboyz/",
@@ -22,7 +22,7 @@ const projectsList = [
     id: 2,
     name: "生活唱片",
     year: 2022,
-    module:"#LifeRecordModal",
+    module:"#Modal",
     github: "https://github.com/vvayHuang/galaxyboyz",
     webkitURL: "https://vvayhuang.github.io/lifeRecord/",
   },
