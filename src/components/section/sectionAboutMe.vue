a188839a9843964e80a8b38be1069615485501fc
vvay <momoopsoops@gmail.com>
2024-11-28 16:58:30 +0800

    refactor: 💡 edit resmume content

--- src/components/section/sectionAboutMe.vue
+++ src/components/section/sectionAboutMe.vue
@@ -29,11 +29,31 @@ const { t, locale } = useI18n();
   </p>
 </div>
 <div class="overflow-hidden mb-3">
-  <p
+  <ul
     class="fs-4 fs-6 fw-bold animate__animated animate__slideInUp animate__slow 3s mb-0"
   >
-  {{t('about me content 4')}}
-  </p>
+  {{t('about me content list')}}
+  <li
+    class="fs-4 fs-6 fw-bold animate__animated animate__slideInUp animate__slow 3s mb-0"
+  >
+  {{t('about me content list item 1')}}
+  </li>
+  <li
+    class="fs-4 fs-6 fw-bold animate__animated animate__slideInUp animate__slow 3s mb-0"
+  >
+  {{t('about me content list item 2')}}
+  </li>
+  <li
+    class="fs-4 fs-6 fw-bold animate__animated animate__slideInUp animate__slow 3s mb-0"
+  >
+  {{t('about me content list item 3')}}
+  </li>
+  <li
+    class="fs-4 fs-6 fw-bold animate__animated animate__slideInUp animate__slow 3s mb-0"
+  >
+  {{t('about me content list item 4')}}
+  </li>
+  </ul>
 </div>
 <img
   class="mb-2 img-fluid"
