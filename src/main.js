9b45099c762a63cdbbc8137e6c2ab11d6d755fe6
wayHuang <88394654+vvayHuang@users.noreply.github.com>
2025-06-10 14:52:13 +0800

    fix: 🐛 modal effect

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import "./assets/scss/main.scss";
-// import * as bootstrap from "bootstrap";
-import "animate.css";
+import './assets/scss/main.scss'
+import * as bootstrap from 'bootstrap'
+import 'animate.css'
 
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import i18n from './i18n';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import i18n from './i18n'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(router);
-app.use(i18n);
-app.mount("#app");
+app.use(router)
+app.use(i18n)
+app.mount('#app')
